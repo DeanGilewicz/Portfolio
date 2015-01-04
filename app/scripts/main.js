ace66f564c2615e27bad7b9a23fb05a8c76de928
Dean Gilewicz <gilewiczd@gmail.com>
2015-01-04 16:44:08 -0500

    updated nav bar to include hamburger

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,18 @@
-$('[data-load-remote]').on('click',function(e) {
-  e.preventDefault();
-  var $this = $(this);
-  var remote = $this.data('load-remote');
-  if(remote) {
-    $($this.data('remote-target')).load(remote);
-  }
-});
+// $('.male').on('click', function () {
+//   $('.social').toggleClass('toggle-show');
+// });
 
-$('.male').on('click', function () {
-  $('.social').toggleClass('toggle-show');
-});
+
+
+//scrolls to targeted id
+// $('body').scrollspy({ target: '.navbar-fixed-top' });
+//
+// //waypoint for animation
+// $('.cd-color-2').waypoint(function(direction) {
+//   $('.img-skill').removeClass('imgnone');
+//   $('.imgleft').addClass('animated bounceInLeft');
+//   $('.imgcenter').addClass('animated bounceIn');
+//   $('.imgright').addClass('animated bounceInRight');
+// }, {
+//   offset: '50%'
+// });
