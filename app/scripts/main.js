6b1e0794026d0e73994748b9161ab61980d2a4dd
Dean Gilewicz <gilewiczd@gmail.com>
2015-01-04 04:07:20 -0500

    changed nav bar to include scrollspy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
 $('#myModal').on('hidden.bs.modal', function () {
   $(this).removeData('modal');
 });
+
+$('.male').on('click', function () {
+  $('.social').toggleClass('toggle-show');
+});
+
+$(document).ready(function(){
+  $('body').scrollspy({target: "#myScrollspy"})
+});
