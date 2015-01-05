93888289d336eaa42a5f8d648f6a93f0c82baf4b
Dean Gilewicz <gilewiczd@gmail.com>
2015-01-04 23:51:09 -0500

    added hover function to social icons in title area

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,15 @@
+
 // $('.male').on('click', function () {
 //   $('.social').toggleClass('toggle-show');
 // });
 
-
+$(function() {
+  $(".wrap").hover(function() {
+    $(this).stop().animate({ top:"-33px" },{ queue:false, duration:200 });
+  }, function() {
+    $(this).stop().animate({ top:"0px" },{ queue:false,duration:200 });
+  });
+});
 
 //scrolls to targeted id
 // $('body').scrollspy({ target: '.navbar-fixed-top' });
