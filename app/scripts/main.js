9924e360a585c1b6e8ce97e11822c3d7f6476a42
Dean Gilewicz <gilewiczd@gmail.com>
2015-01-05 22:25:19 -0500

    added background attachment and included smooth scrolling for links except to get back to top

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,42 @@
 
 // sets an array to be looped through continuously
+// set to allow for change of name and font color
 // $(function () {
 //   var $header = $("#header");
-//   var header = ['Front End Developer', 'Keen Traveler', 'Soccer Enthusiast'];
+//   var header = ['Front End Developer', 'Keen Traveler', 'Front End Developer', 'Soccer Enthusiast'];
+//   var color = ['red', 'blue', 'green', 'white'];
 //   var position = -1;
 //
 //   (function loop() {
 //     position = (position + 1) % header.length;
-//     $header.html(header[position])
+//     // $header.css('color', color)
+//     //   .fadeIn(1000)
+//     //   .delay(2000)
+//     //   .fadeOut(1000);
+//     $header.html(header[position]).css('color', color[position])
 //       .fadeIn(1000)
 //       .delay(2000)
 //       .fadeOut(1000, loop);
 //   })();
 // });
 
+// Performs a smooth page scroll to anchor on same page
+// Set up for all a href links
+$(function() {
+  $('a[href*=#]:not([href=#top])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      || location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+  });
+});
 
 
 // $('.male').on('click', function () {
@@ -23,7 +46,7 @@
 // position the slide in below at exact height to replace current content on hover
 $(function() {
   $(".wrap").hover(function() {
-    $(this).stop().animate({ top:"-55px" },{ queue:false, duration:200 });
+    $(this).stop().animate({ top:"-60px" },{ queue:false, duration:200 });
   }, function() {
     $(this).stop().animate({ top:"30px" },{ queue:false,duration:200 });
   });
